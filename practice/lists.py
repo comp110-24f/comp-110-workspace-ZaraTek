975891cc6d295281d557acd7f7e63c4827b9c6d3
zara.tekmen@gmail.com <zara.tekmen@gmail.com>
2024-09-30 15:02:35 -0400

    List Practice

---
+++ practice/lists.py
@@ -0,0 +1,11 @@
+print(list[str])
+print([])
+names = ["Kris", "Kaki", "Alyssa"]
+print(names[0])
+print(list())
+print(len(names))
+
+names.append("Zara")
+print(names)
+names[3] = "Chloe"
+print(names)
