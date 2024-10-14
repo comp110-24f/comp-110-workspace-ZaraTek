9597053967673868aa21fdc56c45b4da96237765
zara.tekmen@gmail.com <zara.tekmen@gmail.com>
2024-10-14 14:35:00 -0400

    Completed EX04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -5,11 +5,13 @@ __author__ = "730772738"
 
 def all(list: list[int], num: int) -> bool:
     """Returns true if all numbers in list are equal to target number"""
-    same_count: int = 0
+    if len(list) == 0:  # returns false if list is empty
+        return False
+    same_count: int = 0  # tracks how many are the same
     for n in list:
         if n == num:
             same_count += 1
-    if same_count == len(list):
+    if same_count == len(list):  # checks if all values are the same
         return True
     return False
 
@@ -25,5 +27,29 @@ def max(input: list[int]) -> int:
     return max
 
 
+def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """Returns true if lists are equal to each other"""
+    if len(list1) != len(list2):
+        return False
+    same_count: int = 0  # tracks how many are the same
+    for n in range(len(list1)):
+        if list1[n] == list2[n]:
+            same_count += 1
+    if same_count == len(list1):  # checks if all values are the same
+        return True
+    return False
+
+
+def extend(list1: list[int], list2: list[int]) -> None:
+    """Mutates list1 by adding values in list2"""
+    for n in list2:
+        list1.append(n)
+
+
 print(all([1, 1, 2], 1))
 print(max([1, 3, 2]))
+print(is_equal([1, 0, 1], [1, 0, 1, 1]))
+
+list1 = [1]
+extend(list1, [2, 3])
+print(list1)
